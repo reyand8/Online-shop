0cd67ac33d358ee33f32d71c637c7b1b7da1d59b
Andrey <andreeey.gg@gmail.com>
2024-01-18 23:53:40 +0100

    add index.js

---
+++ online-shop/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from "react-router-dom";
+import { Provider } from 'react-redux';
+import App from './components/App'
+import './styles/index.css'
+import {store} from './features/store'
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+)
+
+
+
