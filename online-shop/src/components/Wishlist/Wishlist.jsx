1854e41c0d4f63d3fd0ea35fca513c0145674da4
Andrey <andreeey.gg@gmail.com>
2024-02-16 18:08:23 +0100

    add Wishlist

---
+++ online-shop/src/components/Wishlist/Wishlist.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    addItemToWishlist, removeItemFromWishlist,
+} from "../../features/user/userSlice";
+
+import styles from "../../styles/Basket.module.css";
+import { sumBy } from "../../utils/common";
+
+const Wishlist = () => {
+    const dispatch = useDispatch();
+
+    const { wishlist } = useSelector(({ user }) => user);
+
+    const changeQuantity = (item, quantity) => {
+        dispatch(addItemToWishlist({ ...item, quantity }));
+    };
+
+    const removeItem = (id) => {
+        dispatch(removeItemFromWishlist(id));
+    };
+
+    return (
+        <section className={styles.cart}>
+            <h2 className={styles.title}>Your wishlist</h2>
+            {!wishlist.length ? (
+                <div className={styles.empty}>Your wishlist is empty</div>
+            ) : (
+                <>
+                    <div className={styles.list}>
+                        {wishlist.map((item) => {
+                            const { title, category, images, price, id, quantity } = item;
+                            return (
+                                <div className={styles.item} key={id}>
+                                    <div
+                                        className={styles.image}
+                                        style={{ backgroundImage: `url(${images[0]})` }}
+                                    />
+                                    <div className={styles.info}>
+                                        <h3 className={styles.name}>{title}</h3>
+                                        <div className={styles.category}>{category.name}</div>
+                                    </div>
+                                    <div className={styles.price}>{price}$</div>
+                                    <div className={styles.quantity}>
+                                        <div
+                                            className={styles.minus}
+                                            onClick={() =>
+                                                changeQuantity(item, Math.max(1, quantity - 1))
+                                            }
+                                        >
+                                            <svg className="icon">
+                                                <use
+                                                    xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#minus`}
+                                                />
+                                            </svg>
+                                        </div>
+                                        <span>{quantity}</span>
+                                        <div
+                                            className={styles.plus}
+                                            onClick={() =>
+                                                changeQuantity(item, Math.max(1, quantity + 1))
+                                            }>
+                                            <svg className="icon">
+                                                <use
+                                                    xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#plus`}
+                                                />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <div className={styles.total}>{price * quantity}$</div>
+                                    <div
+                                        className={styles.close}
+                                        onClick={() => removeItem(item.id)}
+                                    >
+                                        <svg className="icon">
+                                            <use
+                                                xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#close`}
+                                            />
+                                        </svg>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div className={styles.actions}>
+                        <div className={styles.total}>
+                            TOTAL PRICE:{" "}
+                            <span>
+                {sumBy(wishlist.map(({ quantity, price }) => quantity * price))}$
+              </span>
+                        </div>
+                        <button className={styles.proceed}>Proceed to checkout</button>
+                    </div>
+                </>
+            )}
+        </section>
+    );
+};
+
+export default Wishlist;
