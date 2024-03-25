9be8cca7d2400d7f6e0c92c1687cd18f0e6d4e1d
Andrey <andreeey.gg@gmail.com>
2024-03-25 17:09:58 +0100

    add product link

--- online-shop/src/components/Basket/Basket.jsx
+++ online-shop/src/components/Basket/Basket.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItemToBasket, removeItemFromBasket,
-} from "../../features/user/userSlice";
+} from '../../features/user/userSlice';
 
-import styles from "../../styles/Basket.module.css";
-import { sumBy } from "../../utils/common";
+import styles from '../../styles/Basket.module.css';
+import { sumBy } from '../../utils/common';
+import {Link} from 'react-router-dom';
 
 const Basket = () => {
     const dispatch = useDispatch();
@@ -30,59 +31,61 @@ const Basket = () => {
                             const { title, category, images, price, id, quantity } = item;
                             return (
                                 <div className={styles.item} key={id}>
-                                    <div
-                                        className={styles.image}
-                                        style={{ backgroundImage: `url(${images[0]})` }}
-                                    />
-                                    <div className={styles.info}>
-                                        <h3 className={styles.name}>{title}</h3>
-                                        <div className={styles.category}>{category.name}</div>
-                                    </div>
-                                    <div className={styles.price}>{price}$</div>
-                                    <div className={styles.quantity}>
+                                    <Link to={`/products/${id}`} key={id} className={styles.item_link}>
                                         <div
-                                            className={styles.minus}
-                                            onClick={() =>
-                                                changeQuantity(item, Math.max(1, quantity - 1))
-                                            }
+                                            className={styles.image}
+                                            style={{ backgroundImage: `url(${images[0]})` }}
+                                        />
+                                        <div className={styles.info}>
+                                            <h3 className={styles.name}>{title}</h3>
+                                            <div className={styles.category}>{category.name}</div>
+                                        </div>
+                                        <div className={styles.price}>{price}$</div>
+                                        <div className={styles.quantity}>
+                                            <div
+                                                className={styles.minus}
+                                                onClick={() =>
+                                                    changeQuantity(item, Math.max(1, quantity - 1))
+                                                }
+                                            >
+                                                <svg className="icon">
+                                                    <use
+                                                        xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#minus`}
+                                                    />
+                                                </svg>
+                                            </div>
+                                            <span>{quantity}</span>
+                                            <div
+                                                className={styles.plus}
+                                                onClick={() =>
+                                                    changeQuantity(item, Math.max(1, quantity + 1))
+                                                }>
+                                                <svg className="icon">
+                                                    <use
+                                                        xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#plus`}
+                                                    />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <div className={styles.total}>{price * quantity}$</div>
+                                        <div
+                                            className={styles.close}
+                                            onClick={() => removeItem(item.id)}
                                         >
                                             <svg className="icon">
                                                 <use
-                                                    xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#minus`}
+                                                    xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#close`}
                                                 />
                                             </svg>
                                         </div>
-                                        <span>{quantity}</span>
-                                        <div
-                                            className={styles.plus}
-                                            onClick={() =>
-                                                changeQuantity(item, Math.max(1, quantity + 1))
-                                            }>
-                                            <svg className="icon">
-                                                <use
-                                                    xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#plus`}
-                                                />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                    <div className={styles.total}>{price * quantity}$</div>
-                                    <div
-                                        className={styles.close}
-                                        onClick={() => removeItem(item.id)}
-                                    >
-                                        <svg className="icon">
-                                            <use
-                                                xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#close`}
-                                            />
-                                        </svg>
-                                    </div>
+                                    </Link>
                                 </div>
                             );
                         })}
                     </div>
                     <div className={styles.actions}>
                         <div className={styles.total}>
-                            TOTAL PRICE:{" "}
+                            TOTAL PRICE:{' '}
                             <span>
                                 {sumBy(basket.map(({ quantity, price }) => quantity * price))}$
                             </span>
