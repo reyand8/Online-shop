c336d40dee2d739cd5a50b407a9113d7c9783f72
Andrey <andreeey.gg@gmail.com>
2024-06-18 17:57:27 +0200

    add babel jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     transform: {
-        // '^.+\\.[t|j]sx?$': 'babel-jest',
+        '^.+\\.[t|j]sx?$': 'babel-jest',
         '^.+\\.js$': 'babel-jest',
     },
     moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx'],
     moduleNameMapper: {
-        "^axios$": "axios/dist/node/axios.cjs"
+        "^axios$": "axios/dist/node/axios.cjs",
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     },
     testEnvironment: 'jsdom',
 };
